f343dd41c087fad66edd4720a26dffa6a98316e9
stencilstrass-spec <stencilstrass@gmail.com>
2026-03-06 00:41:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log("Servidor rodando na porta", PORT);
+app.listen(PORT, () => {
+  console.log("🚀 Servidor rodando na porta", PORT);
 });
